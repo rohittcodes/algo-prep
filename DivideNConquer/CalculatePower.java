6b55a976605f863220c00442e713df01d15cb4f3
Rohitt <rohittcodes@gmail.com>
2025-01-21 10:59:02 +0530

    feat: div-n-con

--- DivideNConquer/CalculatePower.java
+++ DivideNConquer/CalculatePower.java
@@ -1,7 +1,34 @@
 package DivideNConquer;
 
+import java.util.Scanner;
+
 public class CalculatePower {
+  public static int calculatePower(int base, int exp) {
+    if (exp == 0) {
+      return 1;
+    }
+    int temp = calculatePower(base, exp / 2);
+    // If the exponent is even
+    if (exp % 2 == 0) {
+      return temp * temp;
+    } else {
+      if (exp > 0) {
+        // If the exponent is positive
+        return base * temp * temp;
+      } else {
+        // If the exponent is negative
+        return (temp * temp) / base;
+      }
+    }
+  }
+
   public static void main(String[] args) {
-    
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the base: ");
+    int base = sc.nextInt();
+    System.out.println("Enter the exponent: ");
+    int exp = sc.nextInt();
+    System.out.println("Power: " + calculatePower(base, exp));
+    sc.close();
   }
 }
--- DivideNConquer/MajorityElement.java
+++ DivideNConquer/MajorityElement.java
@@ -1,7 +1,50 @@
+/*
+ * Given an array of size n, find the majority element. The majority element is the element that appears more than n/2 times.
+ * You may assume that the array is non-empty and the majority element always exist in the array.
+ */
+
 package DivideNConquer;
 
+import java.util.*;
+
 public class MajorityElement {
+  public static int majorityRec(int[] arr, int low, int high) {
+    if (low == high) {
+      return arr[low];
+    }
+    int mid = low + (high - low) / 2;
+    int left = majorityRec(arr, low, mid);
+    int right = majorityRec(arr, mid + 1, high);
+    if (left == right) {
+      return left;
+    }
+    int leftCount = 0;
+    int rightCount = 0;
+    // Count the number of left and right elements in the array
+    for (int i = low; i <= high; i++) {
+      if (arr[i] == left) {
+        leftCount++;
+      } else if (arr[i] == right) {
+        rightCount++;
+      }
+    }
+    return leftCount > rightCount ? left : right;
+  }
+
+  public static int majorityElement(int[] arr) {
+    return majorityRec(arr, 0, arr.length - 1);
+  }
+
   public static void main(String[] args) {
-    
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number of elements: ");
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+    System.out.println("Enter the elements: ");
+    for (int i = 0; i < n; i++) {
+      arr[i] = sc.nextInt();
+    }
+    System.out.println("Majority Element: " + majorityElement(arr));
+    sc.close();
   }
 }
